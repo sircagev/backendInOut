997659ac628288ddd04fae42790c532def769053
cervantesjuand <juancervantes.1389@gmail.com>
2024-05-18 22:19:14 -0500

    Cambios backend Juan

--- src/routes/Categoria.Router.jdcc.js
+++ src/routes/Categoria.Router.jdcc.js
@@ -5,7 +5,7 @@ import { RegistrarCategoria, ListarCategoria, BuscarCategoria, ActualizarCategor
 const route = Router();
 
 route.post('/registrar', RegistrarCategoria);
-route.get('/listar', validarToken, ListarCategoria);
+route.get('/listar', ListarCategoria);
 route.get('/buscar/:id', BuscarCategoria);
 route.put('/actualizar/:id', ActualizarCategoria);
 route.put('/desactivar/:id', DesactivarCategoria);
